e61dbb52d5c5848eaa8eef12b7ce59325ca1d631
Alan Bausano <alan.bausano@gmail.com>
2023-02-08 12:36:48 -0300

    initial setup, fetch request made correctly, images included in main render of species

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react';
 import './App.scss';
 import Species from './Species';
 
@@ -16,10 +17,64 @@ const SPECIES_IMAGES = {
 const CM_TO_IN_CONVERSION_RATIO = 2.54;
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [charactersWithImage, setCharactersWithImage] = useState(characters);
+  const [loading, setLoading] = useState(false);
+  useEffect(async () => {
+    setLoading(true);
+    await fetch(API_URL)
+      //First api request, the response provides the urls of each species in an array of urls.
+      .then(response => response.json())
+      .then(data => {
+        //Each species url is being fetched and then the response set to a useState array variable.
+        data?.species.map(dataMapped => {
+          console.log(dataMapped);
+          fetch(dataMapped)
+            .then(response => response.json())
+            .then(charactersFetched => {
+              setCharacters(prevCharacters => [
+                ...prevCharacters,
+                charactersFetched,
+              ]);
+              setLoading(false);
+            });
+        });
+      });
+  }, []);
+  useEffect(() => {
+    const imageChar = characters.map(char => {
+      const newChar = {
+        ...char,
+        image:
+          SPECIES_IMAGES[
+            char.name.includes("Yoda's species")
+              ? 'yoda'
+              : char.name.toLowerCase()
+          ],
+      };
+      return newChar;
+    });
+    setCharactersWithImage(imageChar);
+  }, [characters]);
   return (
     <div className="App">
       <h1>Empire Strikes Back - Species Listing</h1>
-      <div className="App-species" />
+      <div className="App-species">
+        {charactersWithImage && !loading
+          ? charactersWithImage.map(character => (
+              <Species
+                key={character.url}
+                name={character.name}
+                classification={character.classification}
+                designation={character.designation}
+                height={character.average_height}
+                numFilms={character.films.length}
+                language={character.language}
+                image={character.image}
+              />
+            ))
+          : 'Loading Characters...'}
+      </div>
     </div>
   );
 }
