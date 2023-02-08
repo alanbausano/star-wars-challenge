554222d9a4a4ac22d951f263afaa2dd1ba168ff2
Alan Bausano <alan.bausano@gmail.com>
2023-02-08 18:41:43 -0300

    characters sorted, height shown in inches

--- src/App.js
+++ src/App.js
@@ -19,7 +19,12 @@ const CM_TO_IN_CONVERSION_RATIO = 2.54;
 function App() {
   const [characters, setCharacters] = useState([]);
   const [charactersWithImage, setCharactersWithImage] = useState(characters);
+  const [sortedChars, setSortedChars] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const convertCmToInches = cm =>
+    cm.includes('n/a') ? 'n/a' : cm / CM_TO_IN_CONVERSION_RATIO;
+
   useEffect(async () => {
     setLoading(true);
     await fetch(API_URL)
@@ -28,7 +33,6 @@ function App() {
       .then(data => {
         //Each species url is being fetched and then the response set to a useState array variable.
         data?.species.map(dataMapped => {
-          console.log(dataMapped);
           fetch(dataMapped)
             .then(response => response.json())
             .then(charactersFetched => {
@@ -41,39 +45,58 @@ function App() {
         });
       });
   }, []);
+
   useEffect(() => {
     const imageChar = characters.map(char => {
+      const newCharactersHeightIn = convertCmToInches(char?.average_height);
       const newChar = {
         ...char,
+        average_height: `${
+          newCharactersHeightIn === 'n/a'
+            ? 'n/a'
+            : `${Math.round(newCharactersHeightIn)}''`
+        }`,
         image:
           SPECIES_IMAGES[
-            char.name.includes("Yoda's species")
+            char?.name.includes("Yoda's species")
               ? 'yoda'
-              : char.name.toLowerCase()
+              : char?.name.toLowerCase()
           ],
       };
       return newChar;
     });
     setCharactersWithImage(imageChar);
   }, [characters]);
+
+  useEffect(() => {
+    const newChars = charactersWithImage?.sort((a, b) => {
+      const aNum = a?.url.split('/');
+      const bNum = b?.url.split('/');
+      const aNumberUrl = aNum[aNum.length - 2];
+      const bNumberUrl = bNum[bNum.length - 2];
+      return aNumberUrl - bNumberUrl;
+    });
+    setSortedChars(newChars);
+  }, [charactersWithImage]);
+
   return (
     <div className="App">
       <h1>Empire Strikes Back - Species Listing</h1>
       <div className="App-species">
-        {charactersWithImage && !loading
-          ? charactersWithImage.map(character => (
+        {charactersWithImage && !loading && charactersWithImage.length > 4
+          ? sortedChars.map(character => (
               <Species
-                key={character.url}
-                name={character.name}
-                classification={character.classification}
-                designation={character.designation}
-                height={character.average_height}
-                numFilms={character.films.length}
-                language={character.language}
-                image={character.image}
+                key={character?.url}
+                name={character?.name}
+                classification={character?.classification}
+                designation={character?.designation}
+                height={character?.average_height}
+                numFilms={character?.films.length}
+                language={character?.language}
+                image={character?.image}
               />
             ))
-          : 'Loading Characters...'}
+          : `Loading Species... ${charactersWithImage.length}/5`}
       </div>
     </div>
   );
